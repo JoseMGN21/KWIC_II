899f4776e55d1902104a8c94ba3f2329237e4b40
Gisela Rocha <gisela.ra@culiacan.tecnm.mx>
2023-03-10 11:12:31 -0700

    circularshift funciona pero ordenamiento aun no

--- src/CircularShift.java
+++ src/CircularShift.java
@@ -1,5 +1,26 @@
 package src;
 
 public class CircularShift {
+
+    public CircularShift() {
+    }
+
+    public void circularShift(Lines inputLines, Lines shiftedLines) {
+        for(int i = 0; i < inputLines.getLines().size(); i++) {
+            String line = inputLines.getLines().get(i);
+            String[] words = line.split(" ");
+            String newLine = "";
+            for(int j = 0; j < words.length; j++) {
+                newLine = "";
+                for(int k = j; k < words.length; k++) {
+                    newLine += words[k] + " ";
+                }
+                for(int k = 0; k < j; k++) {
+                    newLine += words[k] + " ";
+                }
+                shiftedLines.storageLines(newLine);
+            }
+        }
+    }
     
 }
